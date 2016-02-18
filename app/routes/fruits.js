a8e342724784327da7f98e624d49bc194856e28c
Richard Shea <rshea@thecubagroup.com>
2016-02-19 09:55:25 +1300

    Forgot to flesh out fruits route

--- app/routes/fruits.js
+++ app/routes/fruits.js
@@ -1,4 +1,24 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+
+    //Using new ES6 format, otherwise would be model function(){ ...
+    model(){
+        return this.store.findAll('fruit');
+    },
+
+//    setupController(controller, model){
+//        this._super(controller, model);
+//        this.controller.
+//        //debugger;
+//    },
+    actions: {
+        addFruit(newFruit) { 
+            //This should have a .done on the end of it to handle .done/.fail processing
+            //but we skipped over that bit
+            this.store.createRecord('fruit', {name: newFruit.name, price: newFruit.price}).save();
+
+            this.controller.set('newFruit', {});
+        } 
+    }
 });
